e93337c7a3e820b63c3cb76356cce066220980f1
amyik <amyik100@gmail.com>
2023-08-11 14:26:52 +0900

    multi module; service 3개 구성

--- service-a/src/main/java/com/example/opentelemetrypoc/HelloController.java
+++ service-a/src/main/java/com/example/opentelemetrypoc/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.opentelemetrypoc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+  @Value("${spring.application.name:not-set}")
+  private String serviceName;
+
   @GetMapping("say-hello")
   String sayHello() {
-    return "Hello";
+    return "Hello I am " + serviceName;
   }
 }
--- service-a/src/main/java/com/example/opentelemetrypoc/ServiceApplication.java
+++ service-a/src/main/java/com/example/opentelemetrypoc/ServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenTelemetryPocApplication {
+public class ServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenTelemetryPocApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
     }
 
 }
