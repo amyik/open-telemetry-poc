35c82f69025026df126d8e9dd90abee55a30616b
albert <amyik100@gmail.com>
2023-09-12 13:59:09 +0900

    Otel Collector와 Grafana Loki 구성 (#2)
    ## 코드 변경 이유
    - [X] 신규Feature 추가
    - [X] 기존Feature 변경
    - 목적 : Otel Collector의 기본 동작 확인
    
    ## 변경 사항
    - Otel Collector의 단순 log 수집 기능 확인
    - app-a, app-c, app-d의 로그 -> Otel Collector ->  loki -> grafana 로 이동
    - grafana ( node port 30008 ) 을 통해 3 개 app의 로그를 조회 가능하다
    
    ## 참고사항(To Reviewers)
    - tracing관련은 아직 미적용
    
    ## Related Issue and Link

--- app-b/src/main/java/com/example/opentelemetrypoc/HelloController.java
+++ app-b/src/main/java/com/example/opentelemetrypoc/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.opentelemetrypoc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
   @Value("${spring.application.name:not-set}")
@@ -12,11 +14,13 @@ public class HelloController {
 
   @GetMapping("say-hello")
   String sayHello1() {
+    log.info("{} called", serviceName);
     return "Hello I am " + serviceName;
   }
 
   @GetMapping("app-b/say-hello")
   String sayHello2() {
+    log.info("{} called", serviceName);
     return "Hello I am " + serviceName;
   }
 }
