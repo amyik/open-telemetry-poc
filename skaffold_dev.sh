d8fa4d92c1a96cd0696b83b27797624d27cf66bb
amyik <amyik100@gmail.com>
2023-09-13 18:26:00 +0900

    파일 일부 정리

--- skaffold_dev.sh
+++ skaffold_dev.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+# options: https://skaffold.dev/docs/references/cli/#skaffold-dev
 skaffold dev \
 --default-repo=registry.insecure.io:5000 \
---cleanup=false
---wait-for-deletions-max=2m0s
+#--cleanup=false
+#--cleanup=false \
 #--filename=skaffold.yaml \
 #--port-forward=true --wait-for-deletions-max=2m0s \
 #--status-check=true --verbosity info --timestamps=true --kubeconfig=/home/albert/.kube/config
